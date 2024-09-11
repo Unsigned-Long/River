a0d1f78e00855f3c5972896fe6841639aec423c6
ulong2 <shlchen@whu.edu.cn>
2024-09-11 15:08:25 +0800

    fix build git submodule (remove remote update)

--- src/view/viewer.cpp
+++ src/view/viewer.cpp
@@ -1,7 +1,36 @@
-// Copyright (c) 2023. Created on 11/9/23 4:09 PM by shlchen@whu.edu.cn (Shuolong Chen), who received the B.S. degree in
-// geodesy and geomatics engineering from Wuhan University, Wuhan China, in 2023. He is currently a master candidate at
-// the school of Geodesy and Geomatics, Wuhan University. His area of research currently focuses on integrated navigation
-// systems and multi-sensor fusion.
+// River: A Tightly-Coupled Radar-Inertial Velocity Estimator Based on Continuous-Time Optimization
+// Copyright 2024, the School of Geodesy and Geomatics (SGG), Wuhan University, China
+// https://github.com/Unsigned-Long/River.git
+//
+// Author: Shuolong Chen (shlchen@whu.edu.cn)
+// GitHub: https://github.com/Unsigned-Long
+//  ORCID: 0000-0002-5283-9057
+//
+// Purpose: See .h/.hpp file.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * The names of its contributors can not be
+//   used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #include "view/viewer.h"
 #include <utility>
@@ -11,6 +40,10 @@
 #include <vtkPolyData.h>
 #include "util/sampling.hpp"
 #include <vtkOBJReader.h>
+#include "tiny-viewer/object/imu.h"
+#include "tiny-viewer/object/landmark.h"
+#include "tiny-viewer/entity/arrow.h"
+#include "tiny-viewer/entity/line.h"
 
 namespace ns_river {
     std::string Viewer::VR_WIN_NAME = "VR_WIN_NAME";
